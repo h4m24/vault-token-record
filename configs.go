3113a52fb4d1a04a040450def506d74dfe3b6769
h4m24 <eng.hfaouri@gmail.com>
2017-04-19 13:58:37 +0200

    split configs into a different go file

---
+++ configs.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type config struct {
+	VaultAddress string `json:"vaultaddress"`
+	TokensPath   string `json:"tokenspath"`
+	VaultToken   string `json:"vaulttoken"`
+}
+
+func toJSON(p interface{}) string {
+	bytes, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return string(bytes)
+}
+func (p config) toString() string {
+	return toJSON(p)
+}
+
+func getPages() config {
+	raw, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var c config
+	json.Unmarshal(raw, &c)
+	return c
+}
--- main.go
+++ main.go
@@ -8,47 +8,11 @@ first read json config file
 */
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
-)
-
-type config struct {
-	VaultAddress string `json:"vaultaddress"`
-	TokensPath   string `json:"tokenspath"`
-	VaultToken   string `json:"vaulttoken"`
-}
-
-func toJson(p interface{}) string {
-	bytes, err := json.Marshal(p)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
-	return string(bytes)
-}
-func (p config) toString() string {
-	return toJson(p)
-}
-
-func getPages() config {
-	raw, err := ioutil.ReadFile("./config.json")
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
-	var c config
-	json.Unmarshal(raw, &c)
-	return c
-}
+import "fmt"
 
 func main() {
 
 	pages := getPages()
 
-	fmt.Println(toJson(pages))
+	fmt.Println(toJSON(pages))
 }
